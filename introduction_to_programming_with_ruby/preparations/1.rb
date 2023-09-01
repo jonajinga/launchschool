ffea0414c5f9f6dac350eb4e5ba377dc04af768d
Jon Ajinga <127791408+jonajinga@users.noreply.github.com>
2023-09-01 11:54:34 -0600

    Preparations exercises completed.

---
+++ introduction_to_programming_with_ruby/preparations/1.rb
@@ -0,0 +1,20 @@
+# Create a directory named my_folder and then navigate inside that directory. 
+#   Create two files named one.rb and two.rb in the my_folder directory. Write a 
+#   ruby program that outputs the line this is file one when you run the one.rb 
+#   file. Then write another program that outputs this is file two when you run 
+#   the two.rb file. (Hint: one.rb should have this in it puts "this is file 
+#   one").
+
+# In the command line type: mkdir my_folder
+#   Next, create two new files using the touch command: touch one.rb two.rb
+#   Open the file one.rb and create a string that outputs to the console using
+#   the puts command: puts "this is file one"
+#   Open the file two.rb and create a string that outputs to the console using
+#   the puts command: puts "this is file two"
+
+# To run the programs go to the command line and type enter: ruby one.rb or ruby
+#   two.rb
+
+# Note: I didn't create these files but I used the command line to create all of
+#   the files used to complete the exercises in the book.
+
---
+++ introduction_to_programming_with_ruby/preparations/2.rb
@@ -0,0 +1,9 @@
+# When you are finished with the above and both programs are working correctly, 
+#   navigate to the directory above the my_folder directory and delete all of 
+#   the content you generated with one command.
+
+# To navigate to the directory above the my_folder directory enter the 
+#   following into the command line: cd ..
+
+# To delete the my_folder directory and all of its files enter the following
+#   into the command line: rm -r my_folder  
